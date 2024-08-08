fe3b12cd12dae1136bc64191b2519072ceb7ae71
anusha414 <53256022+anusha414@users.noreply.github.com>
2024-08-08 13:58:21 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorHelloWorld;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("app");
