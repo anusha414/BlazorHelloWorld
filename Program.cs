a4f2bbe0b4f32ea4e4a843f928d23635c3cbfae1
anusha414 <53256022+anusha414@users.noreply.github.com>
2024-08-08 14:06:40 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("app");
